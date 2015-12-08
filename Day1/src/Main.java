7ce71f695a344ec0694058320120549e58b3b261
Lars Junker <lars.jun@gmx.de>
2015-12-08 15:11:03 +0100

    Day2/Day1/Day4

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -5,6 +5,20 @@ public class Main {
 
 	public static void main(String[] args) {		
 		System.out.println(StringUtils.countMatches(args[0], "(")-StringUtils.countMatches(args[0], ")"));
+		int floor = 0;
+		boolean reported = false;
+		for (int i = 0; i < args[0].length(); i++) {
+			if (args[0].substring(i, i+1).equals("(")) {
+				floor++;
+			}
+			if (args[0].substring(i, i+1).equals(")")) {
+				floor--;
+				if (!reported && floor < 0) {
+					System.out.println(i);
+					reported = true;
+				}
+			}
+		}
 	}
 
 }
